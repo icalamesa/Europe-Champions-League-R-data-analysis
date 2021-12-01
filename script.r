08e03bf0a265300038ebf10068a2fd765efcfd1f
icalamesa <icalamesa@gmail.com>
2021-12-01 11:14:24 +0100

    implemented filter of matches between German and Spanish teams

--- script.r
+++ script.r
@@ -7,5 +7,5 @@ data <- read.csv("./europe-champions-league/2015-16/champs.csv",header = TRUE)
 
 dataset <- data.frame(data)
 
-elems <- filter(dataset, Stage == "Group")
+elems <- filter(dataset, (grepl("ESP", Team.1) & grepl("GER", Team.2)) | (grepl("GER", Team.1) & grepl("ESP", Team.2)))
 
