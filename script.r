26464fe56a4acf25ab149b89f7288720f1b20e74
icalamesa <icalamesa@gmail.com>
2021-12-03 13:04:59 +0100

    created tables to which work on with numeric data

--- script.r
+++ script.r
@@ -2,19 +2,46 @@ rm(list = ls())
 
 #utilities
 library(dplyr)
+library(stringr)
 source("./utils.r")
 
 data <- read.csv("./europe-champions-league/2015-16/champs.csv",header = TRUE)
 
 dataset <- data.frame(data)
 
-dataset = dataset %>% mutate(score_Team.1=(extract_results(FT, 1)) , score_Team.2=(extract_results(FT, 2)))
+dataset = dataset %>% mutate(FT = str_trim(str_remove(dataset$FT, "\\(\\*\\)")), # Quita (*) y espacio en blanco
+       score_Team.1 = as.numeric(str_split(FT, "-", simplify = TRUE)[, 1]),
+       score_Team.2 = as.numeric(str_split(FT, "-", simplify = TRUE)[, 2]))
 
-spain.matches <- filter(dataset, grepl("ESP", Team.1) | grepl("ESP", Team.2))
-german.matches <- filter(dataset, grepl("GER", Team.1) | grepl("GER", Team.2))
-confrontation.matches <- filter(dataset, (grepl("ESP", Team.1) & grepl("GER", Team.2)) | (grepl("GER", Team.1) & grepl("ESP", Team.2)))
 
+
+
+
+spain.matches <- filter(dataset, 
+                        grepl("ESP", Team.1) | grepl("ESP", Team.2))
+german.matches <- filter(dataset, 
+                        grepl("GER", Team.1) | grepl("GER", Team.2))
+confrontation.matches <- filter(dataset, 
+                        (grepl("ESP", Team.1) & grepl("GER", Team.2)) | (grepl("GER", Team.1) & grepl("ESP", Team.2)))
+
+
+#interesting.data = rbind(interesting.data, confrontation.matches)
 #Now that we have filtered the data of interest, we'll process the data
-
+data.spain = spain.matches %>% transmute( 
+          Stage = Stage,
+          Country = "ESP",
+          Score = case_when(
+            str_detect(Team.1, "ESP") ~ score_Team.1,
+            str_detect(Team.2, "ESP") ~ score_Team.2
+          ),
+          Enemy.score = case_when(
+            str_detect(Team.1, "ESP") ~ score_Team.2,
+            str_detect(Team.2, "ESP") ~ score_Team.1
+          ),
+          Enemy.country = case_when(
+            str_detect(Team.1, "ESP") ~ str_extract(Team.2, "[A-Z]{3}+"),
+            str_detect(Team.2, "ESP") ~ str_extract(Team.1, "[A-Z]{3}+")
+          )
+          )
 
 
