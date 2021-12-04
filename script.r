7be34713fe36aaae527b9fff51ed867d3b7f4b92
icalamesa <icalamesa@gmail.com>
2021-12-04 12:24:15 +0100

    Enemy country parser works

--- script.r
+++ script.r
@@ -19,10 +19,18 @@ dataset = dataset %>% mutate(FT = str_trim(str_remove(dataset$FT, "\\(\\*\\)")),
 
 spain.matches <- filter(dataset, 
                         grepl("ESP", Team.1) | grepl("ESP", Team.2))
-german.matches <- filter(dataset, 
+germany.matches <- filter(dataset, 
                         grepl("GER", Team.1) | grepl("GER", Team.2))
 confrontation.matches <- filter(dataset, 
                         (grepl("ESP", Team.1) & grepl("GER", Team.2)) | (grepl("GER", Team.1) & grepl("ESP", Team.2)))
+
+
+## (b)
+#long <- nchar(x)
+#long
+#country <- substr(x, long - 6, long -4)
+#country
+
 
 
 #interesting.data = rbind(interesting.data, confrontation.matches)
@@ -39,9 +47,31 @@ data.spain = spain.matches %>% transmute(
             str_detect(Team.2, "ESP") ~ score_Team.1
           ),
           Enemy.country = case_when(
-            str_detect(Team.1, "ESP") ~ str_extract(Team.2, "[A-Z]{3}+"),
-            str_detect(Team.2, "ESP") ~ str_extract(Team.1, "[A-Z]{3}+")
+            str_detect(Team.1, "ESP") ~ substr(str_trim(str_split(Team.2, "›", simplify = TRUE)[, 2]), 1, 3),
+            str_detect(Team.2, "ESP") ~ substr(str_trim(str_split(Team.1, "›", simplify = TRUE)[, 2]), 1, 3)
           )
           )
 
+data.germany = germany.matches %>% transmute( 
+  Stage = Stage,
+  Country = "GER",
+  Score = case_when(
+    str_detect(Team.1, "GER") ~ score_Team.1,
+    str_detect(Team.2, "GER") ~ score_Team.2
+  ),
+  Enemy.score = case_when(
+    str_detect(Team.1, "GER") ~ score_Team.2,
+    str_detect(Team.2, "GER") ~ score_Team.1
+  ),
+  Enemy.country = case_when(
+    str_detect(Team.1, "GER") ~ substr(str_trim(str_split(Team.2, "›", simplify = TRUE)[, 2]), 1, 3),
+    str_detect(Team.2, "GER") ~ substr(str_trim(str_split(Team.1, "›", simplify = TRUE)[, 2]), 1, 3)
+  )
+)
 
+#Spain
+mean(data.spain$Score)
+sd(data.spain$Score)
+#Germany
+mean(data.germany$Score)
+sd(data.germany$Score)
